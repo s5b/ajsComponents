1b9ba1610de49120b86e90e8fa710727fd983153
S t u a r t <github@sequation.com.au>
2012-11-25 10:29:09 +1100

    Initial contacts implementation.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,28 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+//function MyCtrl1($scope) {
+//    $scope.contacts = [
+//        { name: 'Barry' },
+//        { name: 'Samantha' },
+//        { name: 'Robert' },
+//        { name: 'Robyn' },
+//        { name: 'Stephen' }
+//    ]
+//}
+//MyCtrl1.$inject = [];
+//
+//
+//function MyCtrl2() {
+//}
+//MyCtrl2.$inject = [];
 
-
-function MyCtrl2() {
+function ContactsCtrl($scope) {
+    $scope.contacts = [
+        { name: 'Barry' },
+        { name: 'Samantha' },
+        { name: 'Robert' },
+        { name: 'Robyn' },
+        { name: 'Stephen' }
+    ]
 }
-MyCtrl2.$inject = [];
--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,14 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).directive('s5bContacts', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/contacts.html'
+        };
+    }).directive('s5bContact', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/contact.html'
+        };
+    });
