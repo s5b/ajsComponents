181d1147051e23983e8461f4bf75fd4204ab5dfc
S t u a r t <github@sequation.com.au>
2012-11-26 07:55:41 +1100

    Clean up of boilerplate code.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,22 +3,6 @@
 /* Controllers */
 
 
-//function MyCtrl1($scope) {
-//    $scope.contacts = [
-//        { name: 'Barry' },
-//        { name: 'Samantha' },
-//        { name: 'Robert' },
-//        { name: 'Robyn' },
-//        { name: 'Stephen' }
-//    ]
-//}
-//MyCtrl1.$inject = [];
-//
-//
-//function MyCtrl2() {
-//}
-//MyCtrl2.$inject = [];
-
 function ContactsCtrl($scope) {
     $scope.contacts = [
         { name: 'Barry' },
